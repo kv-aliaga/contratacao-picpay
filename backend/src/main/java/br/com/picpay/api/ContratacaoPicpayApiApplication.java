4af3e543ed8a71576c146ab8d07a42d1a9258844
Davi Aliaga <kavhsao@gmail.com>
2026-08-18 09:27:07 -0300

    feat/api: inicializa projeto em spring

---
+++ backend/src/main/java/br/com/picpay/api/ContratacaoPicpayApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratacaoPicpayApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ContratacaoPicpayApiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/br/com/picpay/api/ContratacaoPicpayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picpay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratacaoPicpayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
